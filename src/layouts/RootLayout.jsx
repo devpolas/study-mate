aabc108c662215762358d74f71224392a64ee5d9
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-10 23:33:52 +0600

    fix rootlayout padding

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
-    <div className='px-4 sm:px-6 md:px-16 lg:px-24'>
+    <div className='px-4 sm:px-6 md:px-16'>
       <Outlet />
     </div>
   );
