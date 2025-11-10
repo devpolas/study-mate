273dedd47f34a1d49922bcf4d552dda9d3a32a16
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-10 21:50:05 +0600

    rootlayout give some horizoltal padding

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
-    <div>
+    <div className='px-4 sm:px-6 md:px-16 lg:px-24'>
       <Outlet />
     </div>
   );
