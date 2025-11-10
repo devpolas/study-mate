f182d70eb233f261e245139890f528a94904b9f1
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-10 20:19:37 +0600

    add root layout and routing with pages

---
+++ src/routers/router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Homepage from "../pages/Homepage";
+import ProfilePage from "../pages/ProfilePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import CreateProfilePage from "../pages/CreateProfilePage";
+import FindPartnerPage from "../pages/FindPartnerPage";
+import PartnerDetailsPage from "../pages/PartnerDetailsPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      { index: true, Component: Homepage },
+      { path: "login", Component: LoginPage },
+      { path: "signup", Component: RegisterPage },
+      { path: "create-profile", Component: CreateProfilePage },
+      { path: "profile", Component: ProfilePage },
+      { path: "find-partner", Component: FindPartnerPage },
+      { path: "partner/:id", Component: PartnerDetailsPage },
+      { path: "profile", Component: ProfilePage },
+    ],
+  },
+]);
+
+export default router;
