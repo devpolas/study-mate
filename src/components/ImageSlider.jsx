2d31eac877d7e01cc201d6414a4605db114ffa8a
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-12 15:27:55 +0600

    add slider home page and slide three demo image

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -4,26 +4,33 @@ import "swiper/css";
 import "swiper/css/pagination";
 // import required modules
 import { Autoplay, Pagination } from "swiper/modules";
+import SlideCard from "./SlideCard";
+
 export default function ImageSlider() {
   return (
-    <div className='pt-4 relative z-1'>
+    <div className='pt-2 relative z-1'>
       <Swiper
-        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
         slidesPerView={"auto"}
         spaceBetween={5}
         pagination={{
           clickable: true,
         }}
+        speed={3000}
+        loop={true}
+        allowTouchMove
         breakpoints={{
-          320: { slidesPerView: 1 },
-          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 1 },
+          1280: { slidesPerView: 2 },
         }}
         modules={[Pagination, Autoplay]}
         className='mySwiper'
       >
-        <SwiperSlide className='h-[40vf]'>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        {Array.from({ length: 3 }, (_v, i) => (
+          <SwiperSlide>
+            <SlideCard key={i} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
