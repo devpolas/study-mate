6d1632b5d54391941fda67960bb3d9cbfe102e81
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-13 16:56:51 +0600

    inplement all data loader

--- src/utils/dataLoader.js
+++ src/utils/dataLoader.js
@@ -4,7 +4,19 @@ export const getAllUsers = async () => {
   const response = await api.get("/users");
   return response.data;
 };
+export const getSingleUser = async (id) => {
+  const response = await api.get(`/users/${id}`);
+  return response.data;
+};
 export const getMe = async () => {
-  const response = await api.get("/me");
+  const response = await api.get("/users/me");
+  return response.data;
+};
+export const getAllFriend = async (id) => {
+  const response = await api.get(`/friendships/all-friends/${id}`);
+  return response.data;
+};
+export const getAllFriendRequest = async (id) => {
+  const response = await api.get(`/friendships/all-requested-friends/${id}`);
   return response.data;
 };
