d8581a22ba9a7a163e3969898f7d649837f87e61
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-13 16:50:27 +0600

    provide the user context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router";
 import router from "./routers/router";
 import AuthContextProvider from "./context/AuthContextProvider";
+import UserContextProvider from "./context/UserContextProvider";
 
 export default function App() {
   return (
     <AuthContextProvider>
-      <RouterProvider router={router} />
+      <UserContextProvider>
+        <RouterProvider router={router} />
+      </UserContextProvider>
     </AuthContextProvider>
   );
 }
